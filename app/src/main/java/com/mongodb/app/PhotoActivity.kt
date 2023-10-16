8a365f64be1dfae96ef5056b07ada1e0912f31e7
Desokii <desoki.abdelrahman@gmail.com>
2023-10-16 14:35:56 +0200

    handle some bugs in image functionality

--- app/src/main/java/com/mongodb/app/PhotoActivity.kt
+++ app/src/main/java/com/mongodb/app/PhotoActivity.kt
@@ -1,6 +1,11 @@
 package com.mongodb.app
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +15,38 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.mongodb.app.ui.theme.MyApplicationTheme
+import java.io.ByteArrayOutputStream
+import java.net.URLEncoder
+import java.util.UUID
 
 class PhotoActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    var uploadBitmap: Bitmap? = null
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1001
+        private const val CAMERA_REQUEST_CODE = 1002
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme  {
+                auth = FirebaseAuth.getInstance()
+                try{
+                    startActivityForResult(
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE),
+                        CAMERA_REQUEST_CODE
+                    )
+                }
+                catch (e: Exception) {
+                    e.printStackTrace()
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,6 +66,29 @@ fun Greeting3(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Throws(Exception::class)
+private fun uploadImage() {
+    //converting image to bytes to be able to upload it.
+//    val bias = ByteArrayOutputStream()
+//    uploadBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, bias)
+//    val imageInBytes = bias.toByteArray()
+//    val storageRef = Firebase.storage.reference
+//    val newImage = URLEncoder.encode(UUID.randomUUID().toString(), "UTF-8") + ".jpg"
+//    val newImagesRef = storageRef.child("Users/$newImage")
+//    newImagesRef.putBytes(imageInBytes)
+//        .addOnFailureListener { exception ->
+//            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT)
+//                .show()
+//        }.addOnSuccessListener { taskSnapshot ->
+//            newImagesRef.downloadUrl.addOnCompleteListener (
+//                object : OnCompleteListener<Uri> {
+//                override fun onComplete(p0: Task<Uri>) {
+////                    sendItem(p0.result.toString())
+//                }
+//            })
+//        }
+}
+
 
 @Preview(showBackground = true)
 @Composable
